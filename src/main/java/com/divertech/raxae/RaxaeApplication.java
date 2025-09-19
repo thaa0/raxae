e42500516549984b71fff22d83df4bd2c55c1881
thalita <dev.thalita@gmail.com>
2025-09-19 20:11:28 -0300

    init: commit inicial do projeto

---
+++ src/main/java/com/divertech/raxae/RaxaeApplication.java
@@ -0,0 +1,13 @@
+package com.divertech.raxae;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RaxaeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RaxaeApplication.class, args);
+	}
+
+}
