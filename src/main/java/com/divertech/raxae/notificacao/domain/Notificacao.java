488eed6ab45fdeb6b725d48fbd3fe158420e6b4a
Wérick Santos <202411240021@ifba.edu.br>
2025-09-19 23:14:46 -0300

    feat: cria Notificacao no dominio da aplicacao

--- src/main/java/com/divertech/raxae/notificacao/domain/Notificacao.java
+++ src/main/java/com/divertech/raxae/notificacao/domain/Notificacao.java
@@ -6,7 +6,6 @@ import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
-
 import java.time.LocalDateTime;
 import java.util.UUID;
 
