9d96841c838375de181725f2c61e1a942d3cc062
thalita <dev.thalita@gmail.com>
2025-09-19 22:04:43 -0300

    feat: cria Cobranca no dominio da aplicacao

--- src/main/java/com/divertech/raxae/cobranca/domain/Cobranca.java
+++ src/main/java/com/divertech/raxae/cobranca/domain/Cobranca.java
@@ -1,4 +1,37 @@
 package com.divertech.raxae.cobranca.domain;
 
+import com.divertech.raxae.usuario.domain.Usuario;
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@Entity
+@Getter
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
+@NoArgsConstructor
 public class Cobranca {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(columnDefinition = "uuid", updatable = false, unique = true, nullable = false)
+    private UUID id;
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "despesa_id", nullable = false)
+    private Despesa despesa;
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "usuario_id", nullable = false)
+    private Usuario usuario;
+    @Column(precision = 10, scale = 2)
+    private BigDecimal valor;
+    @Enumerated(EnumType.STRING)
+    private StatusCobranca status;
+    private LocalDate dataVencimento;
+    private LocalDate dataPagamento;
+    private LocalDateTime momentoCriacao;
 }
