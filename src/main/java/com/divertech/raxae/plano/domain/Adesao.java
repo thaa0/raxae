8da555607edd09d57a7a83197e343bdbd36933fb
thalita <dev.thalita@gmail.com>
2025-09-19 23:30:47 -0300

    feat: adicona adesao no domino da aplicacao

--- src/main/java/com/divertech/raxae/plano/domain/Adesao.java
+++ src/main/java/com/divertech/raxae/plano/domain/Adesao.java
@@ -1,4 +1,32 @@
 package com.divertech.raxae.plano.domain;
 
+
+import com.divertech.raxae.usuario.domain.Usuario;
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@Entity
+@Getter
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
+@NoArgsConstructor
 public class Adesao {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(columnDefinition = "uuid", updatable = false, unique = true, nullable = false)
+    private UUID id;
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "plano_id", nullable = false)
+    private Plano plano;
+    private UUID idUsuario;
+    @Enumerated(EnumType.STRING)
+    private StatusAdesao statusAdesao;
+    private int diaExpiracao;
+    private LocalDateTime momentoAdesao;
 }
