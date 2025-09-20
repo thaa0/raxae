731b1c433b2bf47c80ee4e4bfd03e7167614ade6
Wérick Santos <202411240021@ifba.edu.br>
2025-09-19 23:11:16 -0300

    feat: cria Notificacao no dominio da aplicacao

--- src/main/java/com/divertech/raxae/notificacao/domain/Notificacao.java
+++ src/main/java/com/divertech/raxae/notificacao/domain/Notificacao.java
@@ -1,4 +1,29 @@
 package com.divertech.raxae.notificacao.domain;
 
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@Entity
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
 public class Notificacao {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(columnDefinition = "uuid", updatable = false, unique = true, nullable = false)
+    private UUID id;
+    @NotNull
+    private UUID userId;
+    @NotBlank
+    private String mensagem;
+    private LocalDateTime dataEnvio;
+    private StatusNotificacao statusNotificacao;
+    private LocalDateTime dataLeitura;
 }
