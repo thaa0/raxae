0dfa6f119c12552b5ea68dc0bd6f711ea13051fd
thalita <dev.thalita@gmail.com>
2025-10-22 20:34:47 -0300

    refactor: refatora codigo e retira metodos nao ultilizados

--- src/main/java/com/divertech/raxae/notificacao/application/service/NotificacaoApplicationService.java
+++ src/main/java/com/divertech/raxae/notificacao/application/service/NotificacaoApplicationService.java
@@ -1,6 +1,5 @@
 package com.divertech.raxae.notificacao.application.service;
 
-import com.divertech.raxae.cobranca.application.port.out.NotificacaoServicePort;
 import com.divertech.raxae.usuario.domain.Usuario;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.stereotype.Service;
@@ -9,10 +8,10 @@ import java.util.List;
 
 @Service
 @Log4j2
-public class NotificacaoServiceAdapter implements NotificacaoServicePort {
+public class NotificacaoApplicationService implements NotificacaoService {
 
     @Override
     public void notificarMembros(List<Usuario> usuarios, String mensagem) {
-        log.info("[NOTIFICACAO MOCK] Notificando {} usuários: {}", usuarios.size(), mensagem);
+        log.info("[NOTIFICACAO] Notificando {} usuários: {}", usuarios.size(), mensagem);
     }
 }
